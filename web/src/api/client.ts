cfd0c7ff1e1474389550ddeba47486b25498b6cb
johntmayo <johntmayo@gmail.com>
2025-12-23 12:00:23 -0500

    f

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -98,7 +98,6 @@ export const api = {
     
     const response = await fetch(url.toString(), {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
     });
     
